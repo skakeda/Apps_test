f28a7ff76020ec0d5e079c18f0e75208021e7830
skakeda <kakeshun14@gmail.com>
2022-06-27 09:25:22 +0900

    first commit

---
+++ main_3.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+        "database/sql"
+        "fmt"
+        "net/http"
+        _ "github.com/go-sql-driver/mysql"
+)
+
+//User is struct
+type User struct {
+        ID   int
+        Name string
+}
+
+func main() {
+        http.HandleFunc("/",HelloHandler)
+        http.ListenAndServe(":8080",nil)
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+
+        db, err := sql.Open("mysql", "root:p5Zrng.)@tcp(127.0.0.1:3306)/sample_db")
+        if err != nil {
+                panic(err.Error())
+        }
+        defer db.Close()
+
+        rows, err := db.Query("SELECT * FROM users")
+        if err != nil {
+                panic(err.Error())
+        }
+        defer rows.Close()
+
+        for rows.Next() {
+                var user User
+                err := rows.Scan(&user.ID, &user.Name)
+                if err != nil {
+                        panic(err.Error())
+                }
+                fmt.Println(user.ID, user.Name)
+                w.Write([]byte(user.Name))
+        }
+
+        err = rows.Err()
+        if err != nil {
+                panic(err.Error())
+        }}
